3d99378abddb617be5f3043bacfbf9db175b64fd
arnau.casas.7e7 <arnau.casas.7e7@itb.cat>
2025-02-10 15:09:31 +0100

    inital again commit

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var juego = TresEnRalla()
+    juego.mostrarTablero()
+}
+
+
+/*
+   val port = 5858
+    // Obrir el servidor
+    val serverSocket = ServerSocket(port)
+    println("Attempting to start socket server...")
+
+    // Obrir la porta a clients
+    val clientSocket = serverSocket.accept()
+    val output = PrintWriter(clientSocket.getOutputStream(), true)
+
+    // Llegir el que diu el robot
+    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream(),"utf8"))
+    val actualPose: String = input.readLine()
+    println("This is my actual pose :$actualPose")
+
+
+    // actual pose to final pose
+    while(true){
+        output.println("(0.222887,0.141989,0.24792,1.23286,-1.20128,-1.22264)")
+
+    }
+
+    //Tancar el server
+    serverSocket.close()
+ */
