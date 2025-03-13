0a742381772f135a04f30f0ea3d65e725f210cdf
arnau <arnau.casas.7e7@itb.cat>
2025-03-13 16:55:39 +0100

    dejado bonico

--- src/Main.kt
+++ src/Main.kt
@@ -54,33 +54,6 @@ fun main() {
     serverSocket.close()
 }
 
-
-/*
-   val port = 5858
-    // Obrir el servidor
-    val serverSocket = ServerSocket(port)
-    println("Attempting to start socket server...")
-
-    // Obrir la porta a clients
-    val clientSocket = serverSocket.accept()
-    val output = PrintWriter(clientSocket.getOutputStream(), true)
-
-    // Llegir el que diu el robot
-    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream(),"utf8"))
-    val actualPose: String = input.readLine()
-    println("This is my actual pose :$actualPose")
-
-
-    // actual pose to final pose
-    while(true){
-        output.println("(0.222887,0.141989,0.24792,1.23286,-1.20128,-1.22264)")
-
-    }
-
-    //Tancar el server
-    serverSocket.close()
- */
-
 fun seleccionarPosiciones(player:String):Pair<Int,Int>{
     var fila = 0
     var columna = 0
